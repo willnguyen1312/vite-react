976e07a7beaff2405eb3c21dab4be7f928f32d22
Nam Nguyen <nam.nguyen@redspace.com>
2023-03-14 18:32:08 -0300

    Wonderful

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { proxy, useSnapshot } from "valtio";
 
+const state = proxy({
+  count: 4,
+  voila: 20,
+});
+const incDur = () => {
+  ++state.count;
+};
+const decDur = () => {
+  --state.count;
+};
 
-function App() {
-  const [count, setCount] = useState(0)
+function First() {
+  const snap = useSnapshot(state);
+  console.log("First rendered")
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h3>{snap.count}</h3>
+      <button disabled={snap.count <= 1} onClick={decDur}>
+        -
+      </button>
+      <button disabled={snap.count >= 10} onClick={incDur}>
+        +
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+function App() {
+  const snap = useSnapshot(state);
+  console.log("App rendered")
+
+  return (
+    <>
+      <First />
+      Voila: {snap.voila}
+    </>
+  );
+}
+
+export default App;
